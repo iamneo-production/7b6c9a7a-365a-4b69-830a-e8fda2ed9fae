bc21c2b197567562afd6c7efe503aafefbc66610
Rohinth <dev@email.com>
2022-04-26 11:21:28 +0530

    Deva pushed backend

---
+++ springapp/src/main/java/com/example/springapp/entities/CartEntity.java
@@ -0,0 +1,100 @@
+package com.example.BaitAndTackleModified.entities;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
+@Entity
+@Table(name = "cart")
+public class CartEntity {
+	
+	public CartEntity() {}
+	
+	public CartEntity(UserEntity user, ProductEntity product, Integer quantity, Integer price) {
+		this.user = user;
+		this.product = product;
+		this.quantity = quantity;
+		this.price = price;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "cart_id")
+	private Long cart_id;
+	
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JoinColumn(name = "user_id", nullable = false)
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	private UserEntity user;
+		
+	
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JoinColumn(name = "product_id", nullable = false)
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	private ProductEntity product;
+	
+	@Column(name = "quantity")
+	private Integer quantity;
+	
+	@Column(name = "price")
+	private Integer price;
+
+	public Long getCart_id() {
+		return cart_id;
+	}
+
+	public void setCart_id(Long cart_id) {
+		this.cart_id = cart_id;
+	}
+
+	public UserEntity getUser() {
+		return user;
+	}
+
+	public void setUser(UserEntity user) {
+		this.user = user;
+	}
+
+	public ProductEntity getProduct() {
+		return product;
+	}
+
+	public void setProduct(ProductEntity product) {
+		this.product = product;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price)
+	{
+		this.price = price;
+	}
+	
+	
+	
+}
---
+++ springapp/src/main/java/com/example/springapp/entities/ProductEntity.java
@@ -0,0 +1,85 @@
+package com.example.BaitAndTackleModified.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "product")
+public class ProductEntity {
+	
+	public ProductEntity() {}
+	
+	public ProductEntity(Long product_id, String productName, Integer price, String productImageUrl,
+	Integer productQuantity) 
+	{
+		this.product_id = product_id;
+		this.productName = productName;
+		this.price = price;
+		this.productImageUrl = productImageUrl;
+		this.stockQuantity = productQuantity;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long product_id;
+	
+	@Column(name = "product_name", nullable = false)
+	private String productName;
+	
+	@Column(name = "product_price", nullable = false)
+	private Integer price; 
+	
+	@Column(name = "product_image_url", nullable = false)
+	private String productImageUrl;
+	
+	@Column(name = "stock_quantity", nullable = false)
+	private Integer stockQuantity;
+
+	public Long getProduct_id() {
+		return product_id;
+	}
+
+	public void setProduct_id(Long product_id) {
+		this.product_id = product_id;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	public String getProductImageUrl() {
+		return productImageUrl;
+	}
+
+	public void setProductImageUrl(String productImageUrl) {
+		this.productImageUrl = productImageUrl;
+	}
+
+	public Integer getStockQuantity() 
+	{
+		return stockQuantity;
+	}
+
+	public void setStockQuantity(Integer productQuantity) 
+	{
+		this.stockQuantity = productQuantity;
+	}
+
+	
+}
